e9493fbd3756f4c2ca2818fe0c4e234bbd1ea5a8
unknown <mallelabalaji@gmail.com>
2024-05-31 13:47:47 +0530

    tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from "./compoents/Card";
+import Color from "./compoents/Color";
+
+function App() {
+  return (
+  <>
+   <Color/>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/compoents/Color.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Color = () => {
+  const [color,setColor]=useState('gray');
+
+ 
+
+  const message= () =>{
+   
+    alert(" Happy anniversary to the most amazing couple! May your love continue to grow stronger with each passing year ,Yours Lucky");
+   }
+    return (
+        <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
+            <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2 '>
+                <div className='flex flex-wrap justify-center mx-96    px-10 gap-3 shadow-lg bg-light px-2 py-3  rounded-2xl bg-white '> 
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black bg-red-500 ' onClick={()=> setColor('red')}>Red</button>
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black   bg-blue-400 ' onClick={()=> setColor('blue')}>Blue</button>
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black  bg-yellow-300 ' onClick={()=> setColor('yellow')}>Yellow</button>
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black  bg-green-400  ' onClick={()=> setColor('green')}>Green</button>
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black bg-purple-600  ' onClick={message}>This is for you Maa</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Color
