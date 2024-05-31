8876352b69c100fc6f6191c2f4218c0f386897cc
Balaji1552 <100054083+Balaji1552@users.noreply.github.com>
2024-05-31 23:00:11 +0530

    Update Color.js

--- src/compoents/Color.js
+++ src/compoents/Color.js
@@ -8,7 +8,7 @@ const Color = () => {
 
   const message= () =>{
    
-    alert(" Happy anniversary to the most amazing couple! May your love continue to grow stronger with each passing year ,Yours Lucky");
+    alert(" here you mention a message");
    }
     return (
         <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
@@ -18,7 +18,7 @@ const Color = () => {
                 <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black   bg-blue-400 ' onClick={()=> setColor('blue')}>Blue</button>
                 <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black  bg-yellow-300 ' onClick={()=> setColor('yellow')}>Yellow</button>
                 <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black  bg-green-400  ' onClick={()=> setColor('green')}>Green</button>
-                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black bg-purple-600  ' onClick={message}>This is for you Maa</button>
+                <button className=' outline-none px-4 py-1 rounded-2xl  shadow-lg text-black bg-purple-600  ' onClick={message}>This is for you my Dear</button>
                 </div>
             </div>
         </div>
